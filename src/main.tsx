491987c5eb5d1143b78a9892e73d09bfbc965dc5
omerkisa1 <omereis.5555@gmail.com>
2026-03-10 16:05:46 +0300

    feat: add dark mode support and hash routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import UIKit from './pages/UIKit.tsx'
+
+function Router() {
+  const hash = window.location.hash
+
+  if (hash === '#/uikit') {
+    return <UIKit />
+  }
+
+  return <App />
+}
+
+// Re-render on hash change
+window.addEventListener('hashchange', () => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <Router />
+    </StrictMode>,
+  )
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router />
   </StrictMode>,
 )
